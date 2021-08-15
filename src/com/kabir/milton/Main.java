9654cba3a101de5504359651bfedb120efadb083
milton-kabir <milton.cse.pust@gmail.com>
2021-08-15 10:25:05 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,16 @@
+//package calculator;
 package com.kabir.milton;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner sc= new Scanner(System.in);
+        String[] ar=sc.nextLine().split(" ");
+        int x=Integer.parseInt(ar[0]);
+        int y=Integer.parseInt(ar[1]);
+        System.out.print(x+y);
     }
 }
