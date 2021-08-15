e591f3940dc0b2670840878327e7c16f544fde48
milton-kabir <milton.cse.pust@gmail.com>
2021-08-15 10:30:28 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -7,10 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner sc= new Scanner(System.in);
-        String[] ar=sc.nextLine().split(" ");
-        int x=Integer.parseInt(ar[0]);
-        int y=Integer.parseInt(ar[1]);
-        System.out.print(x+y);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String[] ar = sc.nextLine().split(" ");
+            if (ar[0].length() == 0) {
+                continue;
+            }
+            if (ar[0].equals("/exit")) {
+                System.out.println("Bye!");
+                return;
+            }
+            int x = Integer.parseInt(ar[0]);
+            if (ar.length == 1) {
+                System.out.println(ar[0]);
+                continue;
+            }
+            int y = Integer.parseInt(ar[1]);
+            System.out.println(x + y);
+        }
+
     }
 }
