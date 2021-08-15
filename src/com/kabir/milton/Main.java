7af0a929e8fadc03a0c183ebeb251443e4ad0791
milton-kabir <milton.cse.pust@gmail.com>
2021-08-15 10:35:07 +0600

    Third Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -13,17 +13,19 @@ public class Main {
             if (ar[0].length() == 0) {
                 continue;
             }
+            if (ar[0].equals("/help")) {
+                System.out.println("The program calculates the sum of numbers");
+                continue;
+            }
             if (ar[0].equals("/exit")) {
                 System.out.println("Bye!");
                 return;
             }
-            int x = Integer.parseInt(ar[0]);
-            if (ar.length == 1) {
-                System.out.println(ar[0]);
-                continue;
+            int s = 0;
+            for (String value : ar) {
+                s += Integer.parseInt(value);
             }
-            int y = Integer.parseInt(ar[1]);
-            System.out.println(x + y);
+            System.out.println(s);
         }
 
     }
